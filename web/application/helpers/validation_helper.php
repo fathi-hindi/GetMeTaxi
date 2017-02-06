a1048da0b68821335e216a3cee3f8ea27b763b68
Fathi Hindi <fathe.hindi@gmail.com>
2017-02-06 18:48:57 +0200

    Adding validation helper and adding it to the auto load helper

---
+++ web/application/helpers/validation_helper.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @Summary: validate email address.
+ * @Author:  Fathi Hindi - 01/27/2017.
+ */
+function isValidEmail($email) {
+	$valid = true;
+	if ($email == "") {
+		$valid = false;
+	}
+	return $valid;
+}
+
+/**
+ * @Summary: validate first name.
+ * @Author:  Fathi Hindi - 01/27/2017.
+ */
+function isValidFirstName($first_name) {
+	$valid = true;
+	if ($first_name == "") {
+		$valid = false;
+	}
+	return $valid;
+}
+
+/**
+ * @Summary: validate last name.
+ * @Author:  Fathi Hindi - 01/27/2017.
+ */
+function isValidLastName($last_name) {
+	$valid = true;
+	if ($last_name == "") {
+		$valid = false;
+	}
+	return $valid;
+}
+
+/**
+ * @Summary: validate phone number.
+ * @Author:  Fathi Hindi - 01/27/2017.
+ */
+function isValidPhone($phone) {
+	$valid = true;
+	if ($phone == "") {
+		$valid = false;
+	}
+	return $valid;
+}
+
+/**
+ * @Summary: validate password.
+ * @Author:  Fathi Hindi - 01/27/2017.
+ */
+function isValidPassword($password) {
+	$valid = true;
+	if ($password == "") {
+		$valid = false;
+	}
+	return $valid;
+}
