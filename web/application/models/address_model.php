f8791ce34ade92c073cf008b95df63c7e0177b43
Fathi Hindi <fathe.hindi@gmail.com>
2017-02-10 08:48:02 +0200

    Address_model : adding 'findAddressById' finder

--- web/application/models/address_model.php
+++ web/application/models/address_model.php
@@ -36,6 +36,26 @@ Class Address_model extends CI_Model {
 		$nick_name = microtime()*1000000000000;
 		return $nick_name;
 	}
+	
+	/**
+     * @Summary: Find address by id.
+     * @Author:  Fathi Hindi.
+	 * @CreationDate: 02/09/2017.
+     */
+    public function findAddressById($address_id) {
+        $condition = "a.address_id='" . $address_id . "'";
+        $this->db->select('a.address_id, a.member_id, a.status, a.is_primary, a.address1, a.address2, a.nick_name, a.orgname, a.last_create, cd.name as city_name');
+        $this->db->from('address as a');
+		$this->db->join('citydesc as cd' , "a.city_id = cd.city_id");
+        $this->db->where($condition);
+        $this->db->limit(1);
+        $query = $this->db->get();
+        if ($query->num_rows() == 1) {
+            return $query->result()[0];
+        } else {
+            return null;
+        }
+    }
 }
 
 ?>
