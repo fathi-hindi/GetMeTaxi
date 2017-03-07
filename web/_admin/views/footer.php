d75ba8df935efd734bb9c55529120da236168beb
Fathi Hindi <fathe.hindi@gmail.com>
2017-03-07 14:39:15 +0200

    Admin: Split admin layout into multiple file.

---
+++ web/_admin/views/footer.php
@@ -0,0 +1,45 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+         <!-- Jquery Core Js -->
+    <script src="/public/admin/plugins/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="/public/admin/plugins/bootstrap/js/bootstrap.js"></script>
+
+    <!-- Select Plugin Js -->
+    <script src="/public/admin/plugins/bootstrap-select/js/bootstrap-select.js"></script>
+
+    <!-- Slimscroll Plugin Js -->
+    <script src="/public/admin/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+
+    <!-- Waves Effect Plugin Js -->
+    <script src="/public/admin/plugins/node-waves/waves.js"></script>
+
+    <!-- Jquery CountTo Plugin Js -->
+    <script src="/public/admin/plugins/jquery-countto/jquery.countTo.js"></script>
+
+    <!-- Morris Plugin Js -->
+    <script src="/public/admin/plugins/raphael/raphael.min.js"></script>
+    <script src="/public/admin/plugins/morrisjs/morris.js"></script>
+
+    <!-- ChartJs -->
+    <script src="/public/admin/plugins/chartjs/Chart.bundle.js"></script>
+
+    <!-- Flot Charts Plugin Js -->
+    <script src="/public/admin/plugins/flot-charts/jquery.flot.js"></script>
+    <script src="/public/admin/plugins/flot-charts/jquery.flot.resize.js"></script>
+    <script src="/public/admin/plugins/flot-charts/jquery.flot.pie.js"></script>
+    <script src="/public/admin/plugins/flot-charts/jquery.flot.categories.js"></script>
+    <script src="/public/admin/plugins/flot-charts/jquery.flot.time.js"></script>
+
+    <!-- Sparkline Chart Plugin Js -->
+    <script src="/public/admin/plugins/jquery-sparkline/jquery.sparkline.js"></script>
+
+    <!-- Custom Js -->
+    <script src="/public/admin/js/admin.js"></script>
+    <script src="/public/admin/js/pages/index.js"></script>
+
+    <!-- Demo Js -->
+    <script src="/public/admin/js/demo.js"></script>
+</body>
+
+</html>
