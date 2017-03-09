a4dada34e9423eda8e25d3082f9dc643dd148a46
Fathi Hindi <fathe.hindi@gmail.com>
2017-03-09 20:26:17 +0200

    Style work for my account setting

--- web/application/views/setting_page.php
+++ web/application/views/setting_page.php
@@ -10,7 +10,7 @@
 		</div>
 		<div class="col-md-9">
 			<div class="row">
-				<div class="col-md-5">
+				<div class="col-md-5 updateAccountForm">
 					<form name="updateAccount">
 						<h4>Account Infomation</h4>
 						<div class="form-group form-group-icon-left">
