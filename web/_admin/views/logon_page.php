35742910fdcfcd693c0984b5dcba09cd284e21b3
Fathi Hindi <fathe.hindi@gmail.com>
2017-03-10 06:33:47 +0200

    Admin: populate fonts localy.

--- web/_admin/views/logon_page.php
+++ web/_admin/views/logon_page.php
@@ -10,8 +10,8 @@
     <link rel="icon" href="/public/admin/favicon.ico" type="image/x-icon">
 
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+    <link href="/public/admin/fonts/css.css" rel="stylesheet">
+    <link href="/public/admin/fonts/icon.css" rel="stylesheet">
 
     <!-- Bootstrap Core Css -->
     <link href="/public/admin/plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
