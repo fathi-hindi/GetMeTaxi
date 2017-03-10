471493597fa0a7b9a101e7bda6987a8e9ef4d8ac
Fathi Hindi <fathe.hindi@gmail.com>
2017-03-10 11:35:37 +0200

    Admin: adding user image constant.

--- web/_admin/config/constants.php
+++ web/_admin/config/constants.php
@@ -83,3 +83,7 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+//OTHERS
+define('USER_PHOTO_FOLDER_PATH', '/public/images/users/');
+define('USER_PHOTO_DEFAULT_IMG', 'default.png');
