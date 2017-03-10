0127203daf9693e7f9382373574d631f7712135e
Fathi Hindi <fathe.hindi@gmail.com>
2017-03-10 14:51:56 +0200

    my account style

--- web/application/views/myaccount_page.php
+++ web/application/views/myaccount_page.php
@@ -11,25 +11,21 @@
 		<div class="col-md-9">
 			<h4>Total Traveled</h4>
 			<ul class="list list-inline user-profile-statictics mb30">
-				<li><i class="fa fa-dashboard user-profile-statictics-icon"></i>
-					<h5>12540</h5>
-					<p>Miles</p>
+				<li><i class="fa fa-truck user-profile-statictics-icon"></i>
+					<h5>140</h5>
+					<p>Orders</p>
 				</li>
-				<li><i class="fa fa-globe user-profile-statictics-icon"></i>
-					<h5>2%</h5>
-					<p>World</p>
+				<li><i class="fa fa-users user-profile-statictics-icon"></i>
+					<h5>123</h5>
+					<p>Users</p>
 				</li>
-				<li><i class="fa fa-building-o user-profile-statictics-icon"></i>
-					<h5>15</h5>
-					<p>Cityes</p>
-				</li>
-				<li><i class="fa fa-flag-o user-profile-statictics-icon"></i>
+				<li><i class="fa fa-car user-profile-statictics-icon"></i>
 					<h5>3</h5>
-					<p>Countries</p>
+					<p>Delivery</p>
 				</li>
-				<li><i class="fa fa-plane user-profile-statictics-icon"></i>
-					<h5>20</h5>
-					<p>Trips</p>
+				<li><i class="fa fa-taxi user-profile-statictics-icon"></i>
+					<h5>2</h5>
+					<p>Taxi Office</p>
 				</li>
 			</ul>
 			<div id="map-canvas" style="width:100%; height:400px;"></div>
