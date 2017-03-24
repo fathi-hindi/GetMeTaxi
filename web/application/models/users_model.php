b88996bd10f4c4654567274bdde64db167c7ca91
Fathi Hindi <fathe.hindi@gmail.com>
2017-03-24 10:34:13 +0300

    Adding user list view and its filters

--- web/application/models/users_model.php
+++ web/application/models/users_model.php
@@ -10,10 +10,46 @@ Class Users_model extends CI_Model {
      */
     public function findAllUsers() {
         
-        $this->db->select('u.users_id, u.user_type, u.date_of_birth, u.first_name, u.last_name, u.middle_name, u.phone, u.fax, u.mobile, u.photo, u.email');
+        $this->db->select('u.users_id, u.user_type, u.date_of_birth, u.first_name, u.last_name, u.middle_name, u.phone, u.fax, u.mobile, u.photo, u.email, ur.status, ur.logon_id');
 		$this->db->from('users as u');
-        
+        $this->db->join('userreg as ur', 'u.users_id = ur.users_id', 'left');
         $query = $this->db->get();
+
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+	
+	/**
+     * @Summary: Find All Users with filters.
+     * @Author:  Fathi Hindi.
+	 * @CreationDate: 03/10/2017.
+     */
+    public function findAllUsersByFilters($filters) {
+        
+        $this->db->select('u.users_id, u.user_type, u.date_of_birth, u.first_name, u.last_name, u.middle_name, u.phone, u.fax, u.mobile, u.photo, u.email, ur.status, ur.logon_id');
+		$this->db->from('users as u');
+        $this->db->join('userreg as ur', 'u.users_id = ur.users_id', 'left');
+        $condition = "";
+		
+		foreach ($filters as $filter) {
+			if ($filter['name'] == 'type') {
+				$condition == "" ? $condition = "u.user_type = '" . $filter['value'] . "'" : $condition .= " and u.user_type = '" . $filter['value'] . "'";
+			} else if ($filter['name'] == 'first_name') {
+				$condition == "" ? $condition = "u.first_name like '%" . $filter['value'] . "%'" : $condition .= " and u.first_name like '%" . $filter['value'] . "%'";
+			} else if ($filter['name'] == 'last_name') {
+				$condition == "" ? $condition = "u.last_name like '%" . $filter['value'] . "%'" : $condition .= " and u.last_name like '%" . $filter['value'] . "%'";
+			} else if ($filter['name'] == 'email') {
+				empty ($condition) ? $condition = "u.email like '%" . $filter['value'] . "%'" : $condition .= " and u.email like '%" . $filter['value'] . "%'";
+			}
+		}
+		
+		if ($condition != "") {
+			$this->db->where($condition);
+		}
+		$query = $this->db->get();
 
         if ($query->num_rows() > 0) {
             return $query->result();
--- web/application/views/users_page.php
+++ web/application/views/users_page.php
@@ -13,53 +13,49 @@
 				<h5>All Users</h5>
 			</div>
 			<div class="row">
-				<div class="col-md-3">
-					<div class="form-group form-group-icon-left">
-						<i class="fa fa-user input-icon input-icon-highlight"></i>
-						<label>First Name</label>
-						<input class="form-control" name="firstName" placeholder="First Name" type="text">
+				<form action="/account/users" method="get" >
+					<div class="col-md-3">
+						<div class="form-group form-group-icon-left">
+							<i class="fa fa-users input-icon input-icon-highlight"></i>
+							<label>User Type</label>
+							<select class="form-control" name="type" >
+								<option value="ALL">All</option>
+									<option value="R">Passenger</option>
+									<option value="G">Guest</option>
+									<option value="A">Admin</option>
+									<option value="O">Office Admin</option>
+									<option value="D">Driver</option>
+							</select>
+						</div>
 					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="form-group form-group-icon-left">
-						<i class="fa fa-user input-icon input-icon-highlight"></i>
-						<label>Last Name</label>
-						<input class="form-control" name="lastName" placeholder="Last Name" type="text">
+					<div class="col-md-3">
+						<div class="form-group form-group-icon-left">
+							<i class="fa fa-user input-icon input-icon-highlight"></i>
+							<label>First Name</label>
+							<input class="form-control" name="firstName" placeholder="First Name" type="text">
+						</div>
 					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="form-group form-group-icon-left">
-						<i class="fa fa-user input-icon input-icon-highlight"></i>
-						<label>Last Name</label>
-						<input class="form-control" name="lastName" placeholder="Last Name" type="text">
+					<div class="col-md-3">
+						<div class="form-group form-group-icon-left">
+							<i class="fa fa-user input-icon input-icon-highlight"></i>
+							<label>Last Name</label>
+							<input class="form-control" name="lastName" placeholder="Last Name" type="text">
+						</div>
 					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="form-group form-group-icon-left">
-						<i class="fa fa-user input-icon input-icon-highlight"></i>
-						<label>Last Name</label>
-						<input class="form-control" name="lastName" placeholder="Last Name" type="text">
+					<div class="col-md-3">
+						<div class="form-group form-group-icon-left">
+							<i class="fa fa-envelope input-icon input-icon-highlight"></i>
+							<label>Email</label>
+							<input class="form-control" name="email" placeholder="Email" type="text">
+						</div>
 					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="form-group form-group-icon-left">
-						<i class="fa fa-user input-icon input-icon-highlight"></i>
-						<label>Last Name</label>
-						<input class="form-control" name="lastName" placeholder="Last Name" type="text">
+					
+					<div class="col-md-3">
+						<div class="form-group form-group-icon-left find-order-button">
+							<button class="btn btn-primary" type="submit" >Find User</button>
+						</div>
 					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="form-group form-group-icon-left">
-						<i class="fa fa-user input-icon input-icon-highlight"></i>
-						<label>Last Name</label>
-						<input class="form-control" name="lastName" placeholder="Last Name" type="text">
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="form-group form-group-icon-left find-order-button">
-						<button class="btn btn-primary" type="button" onclick="CheckoutHelperJS.validateTrackingOrderForm(document.trackingOrderForm);">Find User</button>
-					</div>
-				</div>
+				</form>
 			</div>
 			<hr/>
 			<table class="table table-bordered table-striped table-booking-history">
@@ -67,15 +63,13 @@
 					<tr>
 						<th>Id</th>
 						<th>Type</th>
-						<th>Date Of Birth</th>
+						<th>Logon Id</th>
+						<th>Status</th>
 						<th>First Name</th>
 						<th>Last Name</th>
-						<th>Middle Name</th>
 						<th>email</th>
 						<th>Phone</th>
-						<th>fax</th>
 						<th>mobile</th>
-						<th>photo</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -83,15 +77,13 @@
 					<tr>
 						<td><a href=""><?php echo $user->users_id; ?></a></td>
 						<td><?php echo $user->user_type; ?></td>
-						<td><?php echo $user->date_of_birth; ?></td>
+						<td><?php echo $user->logon_id; ?></a></td>
+						<td><?php echo $user->status; ?></td>
 						<td><?php echo $user->first_name; ?></td>
 						<td><?php echo $user->last_name; ?></td>
-						<td><?php echo $user->middle_name; ?></td>
 						<td><?php echo $user->email; ?></td>
 						<td><?php echo $user->phone; ?></td>
-						<td><?php echo $user->fax; ?></td>
 						<td><?php echo $user->mobile; ?></td>
-						<td><?php echo $user->photo; ?></td>
 					</tr>
 					<?php } ?>
 				</tbody>
